649c8f4fe3faf52e3983ff13af8afc7f560d300c
Matt Rogers <mattrogers@sbcglobal.net>
2014-09-14 16:43:17 -0500

    Add an absurdly simple Sinatra app to start with
    Hook up GitHub OAuth to it because I'm too lazy to do anything else.
    :wink:

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 
 gem "sinatra", "~> 1.4.4", :require => "sinatra/base"
+gem "sinatra_auth_github"
---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'sinatra/base'
+require 'sinatra/auth/github'
+
+module DatWorkflow
+  class App < ::Sinatra::Base
+    enable :sessions
+
+    set :github_options, {
+      :secret => ENV["GITHUB_CLIENT_SECRET"],
+      :client_id => ENV["GITHUB_CLIENT_ID"]
+    }
+
+    register Sinatra::Auth::Github
+
+    get "/" do
+      authenticate!
+      "Hello there, #{github_user.login}"
+    end
+  end
+end
